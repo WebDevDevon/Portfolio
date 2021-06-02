edf62a49f65ad51ecf5730d2d3e0dab22d6d748b
Devon <47128949+WebDevDevon@users.noreply.github.com>
2021-06-01 17:08:20 -0700

    init commit

---
+++ scripts.js
@@ -0,0 +1,73 @@
+window.onscroll = function () { myFunction() };
+
+function myFunction() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("lowBar").style.width = scrolled + "%";
+}
+
+$(document).ready(function () {
+  $('[data-toggle="tooltip"]').tooltip();
+});
+
+const parallax = document.getElementById("parallax");
+
+window.addEventListener("scroll", function () {
+  let offset = window.pageYOffset;
+  parallax.style.backgroundPositionY = offset * 0.6 + "px";
+})
+
+function onScroll(event){
+  var sections = document.querySelectorAll('#menu a');
+  var scrollPos = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop;
+  
+  for( var i = 0; i < sections.length; i++ ){
+    var currLink = sections[i]; 
+    var val = currLink.getAttribute('href');
+    var refElement = document.querySelector(val);
+      if( refElement.offsetTop <= scrollPos && ( refElement.offsetTop + refElement.offsetHeight > scrollPos)){
+        document.querySelector('#menu a').classList.remove('active');
+        currLink.classList.add('active');
+      }else{
+         currLink.classList.remove('active');
+       }
+  } 
+};
+
+window.document.addEventListener('scroll', onScroll );
+
+/* email fun*/
+
+function sendMail(params) {
+  var tempParams = {
+    from_name: document.getElementById("fromName").value,
+    to_name: document.getElementById("toName").value,
+    message: document.getElementById("msg").value,
+  };
+
+  emailjs.send('gmail','template_jvvgjxe',tempParams)
+  .then(function(res){
+    console.log("success", res.status);
+  })
+}
+
+
+/* submit button*/
+
+let button = document.querySelector('.button');
+let buttonText = document.querySelector('.tick');
+
+const tickMark = "<svg width=\"58\" height=\"45\" viewBox=\"0 0 58 45\" xmlns=\"http://www.w3.org/2000/svg\"><path fill=\"#fff\" fill-rule=\"nonzero\" d=\"M19.11 44.64L.27 25.81l5.66-5.66 13.18 13.18L52.07.38l5.65 5.65\"/></svg>";
+
+buttonText.innerHTML = "Submit";
+
+button.addEventListener('click', function() {
+
+  if (buttonText.innerHTML !== "Submit") {
+    buttonText.innerHTML = "Submit";
+  } else if (buttonText.innerHTML === "Submit") {
+    buttonText.innerHTML = tickMark;
+  }
+  this.classList.toggle('button__circle');
+});
